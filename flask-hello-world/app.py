c21290bd10e3ef0bd0bd5082710e6726c2f746ff
vivek6491 <vivekec095@gmail.com>
2025-09-26 08:55:41 +0000

    Update app.py

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -2,5 +2,5 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return jsonify(message="Hello from Azure DevOps CI/CD")
-if __name__ == '__main__':
+if __name__ == '__master__':
     app.run(host='0.0.0.0', port=5000)
