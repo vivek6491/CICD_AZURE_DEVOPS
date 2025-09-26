23044545ed4ebb9ac68a2d006dd9a0f377f15fc0
Ubuntu <azureuser@V1Linux.aqbxqjwnfzxejadxle5yisc52h.cx.internal.cloudapp.net>
2025-09-26 10:33:09 +0000

    app file updated

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -2,6 +2,6 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return jsonify(message="Hello from Azure DevOps CI/CD")
+    return jsonify(message="Hello from Azure DevOps CI/CD - Push 1")
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
